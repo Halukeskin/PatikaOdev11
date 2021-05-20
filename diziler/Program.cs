22816ca7b02f77509c88d26bed8d432c853b35b6
Keskin6161 <76918333+Keskin6161@users.noreply.github.com>
2021-05-20 13:31:12 +0300

    Add files via upload

---
+++ diziler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"kedi", "köpek", "kuş"};
+
+            renkler[0] = "Mavi";
+
+            Console.WriteLine(hayvanlar[0]);
+            Console.WriteLine(renkler[0]);
+
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz:");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("lütfen {0}. sayıyı giriniz", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+
+                int toplam =0;
+                foreach (var sayi in sayiDizisi)
+                {
+                    toplam += sayi;
+                    Console.WriteLine("Ortalama" + toplam/diziUzunlugu);
+                }
+            }
+        }
+    }
+}
